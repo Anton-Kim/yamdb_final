206c24b2101588322597e6125484ac36c4acd2ad
Андреев Антон <obsos32@gmail.com>
2022-09-02 16:00:45 +0300

    Проект спринта 16: CI и CD проекта api_yamdb (v. 1.5)

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS')).split()
 
